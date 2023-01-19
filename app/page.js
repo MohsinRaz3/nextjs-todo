c289a399d735910d73da8146a408af2dd7cd5958
mohsinraz3 <mohsinraz3@gmail.com>
2023-01-19 22:10:58 +0500

    nextjs-todo

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import AddTodo from './addTodo'
+import TodoListing from './todoListing'
+
+export default function Page(){
+  return (
+    //Input Button
+    <div>
+ <AddTodo/> 
+  <TodoListing/>     
+    </div>
+  )
+}
---
+++ app/todoListing.js
@@ -0,0 +1,13 @@
+
+export default function TodoListing() {
+  return (
+    <div>
+    <ul>
+        <li style={{listStyleType: "none", padding: 0}}></li>
+        <li style={{padding: "5px 0"}}>Todo 1</li>
+        <li style={{padding: "5px 0"}}>Todo 2</li>
+        <li style={{padding: "5px 0"}}>Todo 3</li>
+    </ul>
+</div>
+  )
+}
